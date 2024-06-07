6f500a8ee043fec04a1f2b82cd0b82baf97db5f9
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-07 17:53:17 +0800

    feat: redirect user from login page when token does not exist

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
 import './Home.css'
 import Navbar from '../../components/Navbar'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (!token) {
+      navigate('/login', {replace: true})
+    } else {
+      navigate('/', {replace: true})
+    }
+  }, [navigate])
+
   return(
     <>
       <Navbar />
-      <h1>This is the main page</h1>
+      <h1>This is the main page, Welcome</h1>
     </>
   )
 }
