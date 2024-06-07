d6703a338ebf54bc53231811343755e3e1b06c04
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-07 14:17:05 +0800

    refactor: login sending post values

--- src/assets/services/login.js
+++ src/assets/services/login.js
@@ -3,18 +3,13 @@ import axios from 'axios'
 const baseURL = 'http://127.0.0.1:8000/api/login'
 
 const Login = async (username, password) => {
-  try {
-    const login = await axios.post(baseURL, {username, password}, {
+  const response = await axios.post(baseURL, {username, password}, {
       headers: {
         'Content-Type': 'application/json'
       }
     })
 
-    return login.data.token
-    
-  } catch(error) {
-    return error.response.data
-  }
+  return response.data
 }
 
 export default Login
