e87fe81431a6e71924f9e25092a71d55fc86144a
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-19 14:39:07 +0800

    feat: redirect to buy page with id parameter

--- src/assets/components/Items.jsx
+++ src/assets/components/Items.jsx
@@ -1,16 +1,19 @@
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import { useNavigate } from 'react-router-dom';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const onBuyNow = (e) => {
-  console.log(e)
-}
-
-
 const Items = ({id, name, price}) => {
+
+  const navigate = useNavigate()
+
+  const onBuyNow = (e) => {
+    navigate(`/buy?id=${e.target.id}`)
+  }
+
   return(
     <>
       <Box>
@@ -25,7 +28,7 @@ const Items = ({id, name, price}) => {
           </CardContent>
           <CardActions>
             <Button size="small">Add to cart</Button>
-            <Button size="small" onClick={onBuyNow} >Buy now</Button>
+            <Button size="small" id={id} onClick={onBuyNow} >Buy now</Button>
           </CardActions>
         </Card>
       </Box>
